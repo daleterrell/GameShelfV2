6b9e056c3e5b6af359b47830bd267836bc37de46
Dale Terrell <tragicid@gmail.com>
2020-09-08 14:13:02 -0500

    more

--- backend/src/main/gameshelf/publisher/PublisherRepository.java
+++ backend/src/main/gameshelf/publisher/PublisherRepository.java
@@ -1,3 +1,7 @@
-public class PublisherRepository {
+package org.aim.movie.publisher;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Integer> {
 
 }
