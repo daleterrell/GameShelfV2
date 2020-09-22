67c4dc932566de800b405e49b9b116085e2ee0de
Dale Terrell <tragicid@gmail.com>
2020-09-22 18:50:09 -0500

    frontend left

--- backend/src/main/gameshelf/title/TitleRepository.java
+++ backend/src/main/gameshelf/title/TitleRepository.java
@@ -1,8 +1,12 @@
 package org.aim.gameshelf.title;
 
+import org.aim.gameshelf.publisher;
+import org.aim.gameshelf.designer;
+
+
 import org.springframework.data.repository.CrudRepository;
 
-import org.aim.movie.movie.Game;
+import org.aim..gameshelf;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
 
